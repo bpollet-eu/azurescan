01cb3a15a95ef80d5f9d825210f591d8cc532e70
bpollet-eu <121290558+bpollet-eu@users.noreply.github.com>
2026-07-30 20:33:48 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: "AzureScan" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
- return (<html lang="nl"><body>{children}</body></html>);
-}
