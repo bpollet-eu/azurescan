75b325ba07c3719115e68bef0523f6db25fa6006
bpollet-eu <121290558+bpollet-eu@users.noreply.github.com>
2026-07-30 18:53:11 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: "AzureScan" };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+ return (<html lang="nl"><body>{children}</body></html>);
+}
